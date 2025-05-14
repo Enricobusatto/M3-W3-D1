95f78de585204e73f874ae034c5085d3e9aeef5e
Enricobusatto <enricobusatto75@gmail.com>
2025-05-14 11:14:10 +0200

    inizio

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    // document.cookie = "username=John Doe";
+    let x = document.cookie;
+    let miastringa = x.split('=');
+    if (miastringa[0] =="username" && miastringa[1] == "John Doe") {
+        document.getElementById('login').classList.add('d-none')
+    }else{
+        document.getElementById('menuUtente').classList.add('d-none')
+    }
+} 
